ab61819f8f603ecdff975eeb0283a310435873ef
Treille Morgane <treillemorgane@gmail.com>
2018-07-27 14:34:54 +0200

    ajout de plugins

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -6,8 +6,8 @@ import com.github.lalyos.jfiglet.FigletFont;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+	
     public static void main( String[] args )
     {
        String asciiArt = FigletFont.convertOneLine("Hello");
